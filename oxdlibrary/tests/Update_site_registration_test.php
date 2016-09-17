36d6df99d6f5ece1bb6b69e7c6ab40a9100ec52f
dollar007 <vlad.karapetyan.1988@mail.ru>
2016-09-17 21:05:32 +0400

    test examples

--- oxdlibrary/tests/Update_site_registration_test.php
+++ oxdlibrary/tests/Update_site_registration_test.php
@@ -4,7 +4,6 @@ session_start();
 include_once '../Update_site_registration.php';
 
 $update_site_registration = new Update_site_registration();
-
 $update_site_registration->setRequestAcrValues(Oxd_RP_config::$acr_values);
 $update_site_registration->setRequestOxdId($_SESSION['oxd_id']);
 $update_site_registration->setRequestAuthorizationRedirectUri(Oxd_RP_config::$authorization_redirect_uri);
@@ -13,8 +12,6 @@ $update_site_registration->setRequestContacts(["test@test.test"]);
 $update_site_registration->setRequestGrantTypes(Oxd_RP_config::$grant_types);
 $update_site_registration->setRequestResponseTypes(Oxd_RP_config::$response_types);
 $update_site_registration->setRequestScope(Oxd_RP_config::$scope);
-
 $update_site_registration->request();
-
 print_r($update_site_registration->getResponseObject());
 
